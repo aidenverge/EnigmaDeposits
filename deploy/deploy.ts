45b8fda121ef48d3beb8d275c66b6c034b70cad4
aidenverge <gdekyju7089@outlook.com>
2025-11-30 15:26:55 +0800

    Update deposit

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -3,15 +3,35 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
-  const { deploy } = hre.deployments;
+  const { deploy, save, getExtendedArtifact } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const cusdt = await deploy("ConfidentialUSDT", {
     from: deployer,
+    gasLimit: 6_000_000,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const fundraiserFactory = await hre.ethers.getContractFactory("EnigmaFundraising");
+  const fundraiserContract = await fundraiserFactory.deploy(cusdt.address, {
+    gasLimit: 6_000_000,
+  });
+  const fundraiserDeploymentTx = fundraiserContract.deploymentTransaction();
+  const fundraiserReceipt = fundraiserDeploymentTx ? await fundraiserDeploymentTx.wait() : undefined;
+  const fundraiserAddress = await fundraiserContract.getAddress();
+
+  const fundraiserArtifact = await getExtendedArtifact("EnigmaFundraising");
+
+  await save("EnigmaFundraising", {
+    abi: fundraiserArtifact.abi,
+    address: fundraiserAddress,
+    transactionHash: fundraiserDeploymentTx?.hash,
+    receipt: fundraiserReceipt,
+    args: [cusdt.address],
+  });
+
+  console.log(`ConfidentialUSDT contract: ${cusdt.address}`);
+  console.log(`EnigmaFundraising contract: ${fundraiserAddress}`);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_fundraiser"; // id required to prevent reexecution
+func.tags = ["ConfidentialUSDT", "EnigmaFundraising"];
